81adb0594e38b28381aefdfb3f0a8d779f563b8e
taneeshaa <kalrataneesha@gmail.com>
2024-05-26 13:36:33 +0530

    Dungeon Quest

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -7,9 +7,17 @@ public class PlayerInteractions : MonoBehaviour
     public GameObject promptUI;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "potionMaker")
+        if (other.gameObject.tag == "potionMaker" || other.gameObject.tag == "treasureChest")
         {
             promptUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "potionMaker" || other.gameObject.tag == "treasureChest")
+        {
+            promptUI.SetActive(false);
         }
     }
 }
