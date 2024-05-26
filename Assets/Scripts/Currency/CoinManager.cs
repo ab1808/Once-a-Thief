c4d23da3bff504c8d9b3b71561b92ea99db746bf
AASHRAY BHALLA <aashraybhalla81@gmail.com>
2024-05-26 17:18:10 +0530

    Currency and Shop System Implemented

---
+++ Assets/Scripts/Currency/CoinManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+	public class CoinManager : MonoBehaviour
+	{
+		private int coinCount;
+
+		// Event to update the UI or any other listeners
+		public delegate void OnCoinCountChanged(int newCoinCount);
+		public event OnCoinCountChanged onCoinCountChanged;
+
+		private void Start()
+		{
+			coinCount = 1000; // Initial coin count
+			onCoinCountChanged?.Invoke(coinCount);
+		}
+
+		public void IncrementCoins(int amount)
+		{
+			coinCount += amount;
+			onCoinCountChanged?.Invoke(coinCount);
+		}
+
+		public void DecrementCoins(int amount)
+		{
+			if (coinCount - amount >= 0)
+			{
+				coinCount -= amount;
+				onCoinCountChanged?.Invoke(coinCount);
+			}
+			else
+			{
+				Debug.LogWarning("Not enough coins to decrement");
+			}
+		}
+
+		public int GetCoinCount()
+		{
+			return coinCount;
+		}
+	}
